8df48d995130031e32f5590f96832d63b1b9653f
sebzviesmaravilha <109434319+sebzviesmaravilha@users.noreply.github.com>
2024-10-26 17:01:52 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,6 +22,41 @@ export default function About() {
       </Head>
       <Navbar />
 
+      <h3 className="h-color">Asaf 'Maravilha'</h3>
+
+      <div className="about-container">
+          <div className="about-card">
+            <div>
+              <h4 className="h-color">Uygun Fiyat</h4>
+              <p className="p-color">
+                Hizmetlerimiz piyasada bulunan insanların rahatlıkla elde edebilmesi için uygun fiyatlı tutuluyor.
+              </p>
+              <hr />
+            </div>
+            <div>
+              <h4 className="h-color">7/24 Destek</h4>
+              <p className="p-color">
+                Herhangi bir sorunuz olduğunda veya yardıma ihtiyaç duyduğunuzda, 7/24 ulaşabileceğiniz destek ekibimiz her zaman yanınızda.
+              </p>
+              <hr />
+            </div>
+            <div>
+              <h4 className="h-color">Kişiselleştirilebilir Özellikler</h4>
+              <p className="p-color">
+                Botlarımız tamamen özelleştirilebilir olup, ihtiyacınıza göre düzenlenebilir. Üstelik bazı özel kişiselleştirme seçenekleri tamamen ÜCRETSİZ!
+              </p>
+              <hr />
+            </div>
+            <div>
+              <h4 className="h-color">Güvenilir ve Hızlı</h4>
+              <p className="p-color">
+                Hizmetlerimiz güvenilir sunucularda barındırılmakta olup hızlı, sorunsuz ve güvenli bir deneyim sunmaktadır.
+              </p>
+              <hr />
+            </div>
+          </div>
+        </div>
+
       <section className="about-section">
         <h1 className="about-title">Asaf 'Maravilha'</h1>
         <p className="about-intro">
