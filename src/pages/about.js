4da9f28c8cf9a0f8dfedd0989d868d17cdaf2bb3
sebzviesmaravilha <109434319+sebzviesmaravilha@users.noreply.github.com>
2024-10-26 18:16:21 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,9 +26,16 @@ export default function About() {
         <div className="about-container">
           <div className="about-card">
             <div>
-              <h4 className="h-color">Asaf 'Maravilha'</h4>
+              <h4 className="h-color"></h4>
               <p className="p-color">
-                Hizmetlerimiz piyasada bulunan insanların rahatlıkla elde edebilmesi için uygun fiyatlı tutuluyor.
+              Merhaba! Ben Maravilha, yazılım geliştirme, topluluk yönetimi ve projelerle insanların hayatlarını kolaylaştırma konusunda tutkuyla çalışan bir geliştiriciyim. Teknoloji dünyasına olan ilgim, özellikle Discord botları, oyun sunucuları ve otomasyon çözümleri gibi alanlarda özgün projeler üretmemi sağlıyor. Yazılım yolculuğum boyunca, ücretli ve ücretsiz projelerle birçok farklı ihtiyaç ve talebe çözüm sunmaya odaklandım. Geliştirdiğim projeler arasında, toplulukların büyümesine yardımcı olacak çok yönlü botlardan, oyun sunucuları için özel olarak tasarlanmış araçlara kadar çeşitli çözümler yer alıyor. Her projede kullanıcı odaklı ve sürdürülebilir çözümler üretmeyi amaçlıyorum.
+              </p>
+              <hr />
+            </div>
+            <div>
+              <h4 className="h-color">Misyonum</h4>
+              <p className="p-color">
+              Amacım, yazılım ve teknolojiyi kullanarak insanların çevrimiçi deneyimlerini zenginleştirmek ve kolaylaştırmak. Sunduğum her hizmet, işlevselliği kadar kullanıcı dostu olmasına da özen gösterilerek tasarlanıyor.
               </p>
               <hr />
             </div>
